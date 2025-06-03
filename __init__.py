68c8fc36ac0d8ca79c3b4e1f6ea9d4345d873c1c
nypox <hello@nypox.com>
2025-06-03 15:16:53 +0000

    auto update

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+from loguru import logger
+from huggingface_hub import hf_hub_download as orig_hf_hub_download
+def patched_hf_hub_download(model_id, filename, revision=None, cache_dir=None, local_dir=None, *args, **kwargs):
+    logger.info(f"hf download {model_id} {filename}")
+    if "EVA-CLIP" in model_id:
+        local_dir = "/root/.cache/eva-clip"
+    return orig_hf_hub_download(
+        model_id, filename, revision=revision, cache_dir=cache_dir, local_dir=local_dir, *args, **kwargs
+    )
+import huggingface_hub
+huggingface_hub.hf_hub_download = patched_hf_hub_download
+
 # only import if running as a custom node
 from .nodes.lora import NunchakuFluxLoraLoader
 from .nodes.models import (
